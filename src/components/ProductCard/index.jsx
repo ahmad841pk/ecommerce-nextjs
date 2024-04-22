7bc2fdf81f32ffffb51d83a11c5ea08611eb2709
Ahmad <contact.mahmad1@gmail.com>
2024-04-22 19:16:26 +0500

    uncomment button

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -78,14 +78,14 @@ export default function ProductCard({ selectedCategories, selectedPriceRanges, s
                 <div className="mx-auto max-w-2xl px-4 py-10 sm:px-6 lg:max-w-7xl lg:px-8">
                     <div className="flex justify-end">
 
-                        {/* <button
+                        <button
                         type="button"
                         className="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         onClick={() => setOpen(!open)}
                     >
                         Add new Product
                         <PlusCircleIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
-                    </button> */}
+                    </button>
                     </div>
                     <DialogModal open={open} setOpen={setOpen} fetchProducts={fetchProducts} />
                     <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
